98c3426cdfb0815ec01de42098f3c1369180b6f8
Hiba Ibrahim Khanfar <hibaibraheem140@gmail.com>
2026-03-06 02:23:05 +0200

    Add WeatherResponseDto DTO for weatherAPI response

---
+++ Wasel_Palestine.PL/DTOs/WeatherResponseDto.cs
@@ -0,0 +1,14 @@
+namespace Wasel_Palestine.PL.DTOs
+{
+    
+ public class WetherResponseDto
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string? Temperature { get; set; }
+        public string? Condition { get; set; }
+        
+
+    }
+
+}
