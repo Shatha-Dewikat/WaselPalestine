342c8262686405dc48e0d392fc4ce0b02f12fad2
Shatha Dwikat <sdwikat93@gmail.com>
2026-03-07 16:33:34 +0200

    Update on dataBase

--- Wasel_Palestine.DAL/Model/IncidentHistory.cs
+++ Wasel_Palestine.DAL/Model/IncidentHistory.cs
@@ -16,7 +16,7 @@ namespace Wasel_Palestine.DAL.Model
 
         public DateTime ChangedAt { get; set; }
 
-        public int ChangedByUserId { get; set; }
+        public String ChangedByUserId { get; set; }
         public User ChangedByUser { get; set; }
     }
 }
